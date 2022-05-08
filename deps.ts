a247166ed8c29022f5e362a4f184de20bded6a6a
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-05-08 17:01:51 +0400

    Update model

--- deps.ts
+++ deps.ts
@@ -1,3 +1,17 @@
-export * as TensorFlow from "https://cdn.skypack.dev/@tensorflow/tfjs@v3.16.0";
-export * as Canvas from "https://deno.land/x/canvas@v1.4.1/mod.ts";
+import * as TensorFlow from "https://cdn.skypack.dev/@tensorflow/tfjs@v3.16.0?dts";
 export * as PNG from "https://deno.land/x/pngs@0.1.1/mod.ts";
+
+// TensorFlow backends
+if ("GPU" in window) {
+  await import("https://cdn.skypack.dev/@tensorflow/tfjs-backend-webgpu");
+  await TensorFlow.setBackend("webgpu");
+  await TensorFlow.ready();
+} else {
+  /* someday this will work...
+    await import("https://cdn.skypack.dev/@tensorflow/tfjs-backend-wasm");
+    await TensorFlow.setBackend("wasm");
+    await TensorFlow.ready();
+  */
+}
+
+export { TensorFlow };
--- model.ts
+++ model.ts
@@ -1,7 +1,6 @@
 import { TensorFlow } from "./deps.ts";
 
-const MODEL =
-  "https://raw.githubusercontent.com/andre4ik3/anime-ban/master/model";
+const MODEL = "https://teachablemachine.withgoogle.com/models/UEQoEtQtI";
 
 // For compatibility, use custom http handler and enable local storage
 const http = TensorFlow.io.http(`${MODEL}/model.json`, { fetchFunc: fetch });
